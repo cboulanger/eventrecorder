5c226b387765f6ae2abc3659db91cc38300a2920
Christian Boulanger <info@bibliograph.org>
2019-03-28 07:37:13 +0100

    Fixing qx serve URL in test

--- tests/testcafe.js
+++ tests/testcafe.js
@@ -1,7 +1,7 @@
 //import {IdSelector, QxSelector} from "../npm/adapters/TestCafe";
 
 fixture `Testing demo`
-  .page `http://127.0.0.1:8080/build/eventrecorder/index.html`;
+  .page `http://127.0.0.1:8080/compiled/source/eventrecorder/index.html`;
 
 test('Test the eventrecorder presentation', async t => {
   await t.wait(10000);
