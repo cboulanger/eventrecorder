f659f6c8962a3932b3a7c24d1a44e7a689638704
Christian Boulanger <info@bibliograph.org>
2019-08-31 18:18:40 +0200

    Add channel id

--- source/class/qx/io/channel/Channel.js
+++ source/class/qx/io/channel/Channel.js
@@ -18,18 +18,24 @@ qx.Class.define("qx.io.channel.Channel", {
   /**
    * Constructor
    * @param transport {qx.data.channel.transport.ITransport}
+   * @param channelId {String?}
+   *    The channel id is needed in situations where multiple channels share a
+   *    transport object.
    */
-  construct: function(transport) {
+  construct: function(transport, channelId) {
     this.base(arguments);
     qx.Interface.objectImplements(transport, qx.data.channel.transport.ITransport);
     this.__transport = transport;
+    this.__channelId = channelId;
 
     // retransmit message, attaching channel information
     transport.addListener("message", function(e) {
       var message = e.getData();
-      message.channel = this;
-      this.fireDataEvent("message", message);
-    });
+      if (!message.$$channelId || message.$$channelId === this.__channelId) {
+        message.$$channel = this;
+        this.fireDataEvent("message", message);
+      }
+    },this);
 
     // bind the status of the channel to the status of the transport
     this.initStatus(transport.getStatus());
@@ -48,6 +54,10 @@ qx.Class.define("qx.io.channel.Channel", {
   },
 
   properties: {
+
+    /**
+     * The status of the channel
+     */
     status: {
       check: [
         qx.io.channel.Channel.CHANNEL_STATUS_OPEN,
@@ -56,18 +66,22 @@ qx.Class.define("qx.io.channel.Channel", {
         qx.io.channel.Channel.CHANNEL_STATUS_OFFLINE
       ],
       deferredInit: true,
-      event: "changeStatus",
-      apply: "_applyStatus"
+      event: "changeStatus"
     }
   },
 
   members: {
 
+    /**
+     * The transport used
+     * @var {qx.io.channel.transport.ITransport}
+     */
     __transport: null,
 
-    _applyStatus: function(value, old) {
-      // prevent non-transport objects from changing the status?
-    },
+    /**
+     * An identifier which needs to be unique in each of the endpoint contexts
+     */
+    __channelId: null,
 
     /**
      * Returns the transport object
@@ -78,10 +92,21 @@ qx.Class.define("qx.io.channel.Channel", {
     },
 
     /**
+     * Returns the channel id or null if none has been set
+     * @return {String|null}
+     */
+    getChannelId: function() {
+      return this.__channelId;
+    },
+
+    /**
      * Send a message into the channel, using the selected transport
      * @param messageObj {Object}
      */
     sendMessage: function (messageObj) {
+      if (this.__channelId) {
+        messageObj.$$channelId = this.__channelId;
+      }
       this.__transport.sendMessage(messageObj);
     }
   }
